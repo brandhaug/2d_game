b3f9480144b0b4092e1daa7d54c3a0633b8d16c6
Martin Brandhaug <mbrandhaug@live.no>
2018-02-05 16:58:48 +0100

    - Highscores created - Added styleclasses

---
+++ src/Highscores/HighscoresController.java
@@ -0,0 +1,33 @@
+package Highscores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HighscoresController {
+
+    @FXML private Button mainMenuButton;
+
+    @FXML
+    protected void openMainMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../MainMenu/MainMenu.fxml"));
+        root.getStylesheets().add(getClass().getResource("../styles.css").toExternalForm());
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void initialize() {
+    }
+
+}
--- src/MainMenu/MainMenuController.java
+++ src/MainMenu/MainMenuController.java
@@ -1,9 +1,16 @@
 package MainMenu;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.prefs.Preferences;
 
 public class MainMenuController {
@@ -35,8 +42,14 @@ public class MainMenuController {
     }
 
     @FXML
-    protected void openHighscores() {
-        System.out.println("Highscors");
+    protected void openHighscores(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../Highscores/Highscores.fxml"));
+        root.getStylesheets().add(getClass().getResource("../styles.css").toExternalForm());
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
