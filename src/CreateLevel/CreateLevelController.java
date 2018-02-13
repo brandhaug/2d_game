39575fadae72b688fd15371bb58f3e71e02b4c36
Martin Brandhaug <mbrandhaug@live.no>
2018-02-13 22:38:11 +0100

    - Added buttons for createLevel - Created GUI for createLevel - Added methods and FXML items in createLevel controller

--- src/CreateLevel/CreateLevelController.java
+++ src/CreateLevel/CreateLevelController.java
@@ -1,13 +1,24 @@
 package CreateLevel;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 
 public class CreateLevelController {
 
-    @FXML private Canvas canvas;
+    @FXML
+    private Canvas canvas;
+    @FXML
+    Button tileButton;
+    @FXML
+    Button enemyButton;
+    @FXML
+    Button coinButton;
+    @FXML
+    Button loadButton;
 
     private GraphicsContext gc;
     private final int GRID_SIZE = 128;
@@ -26,4 +37,15 @@ public class CreateLevelController {
         }
     }
 
+    public void chooseTile() {
+    }
+
+    public void chooseCoin() {
+    }
+
+    public void chooseEnemy() {
+    }
+
+    public void openLoadFile() {
+    }
 }
