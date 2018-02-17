20aee98abec215f1e8f65634c3171722be43a698
brandhaug-desktop <mbrandhaug@live.no>
2018-02-17 20:50:04 +0100

    - Buttons

--- src/CreateLevel/CreateLevelController.java
+++ src/CreateLevel/CreateLevelController.java
@@ -23,6 +23,14 @@ public class CreateLevelController {
     private GraphicsContext gc;
     private final int GRID_SIZE = 128;
 
+
+    private final int TILE_ENABLED = 0;
+    private final int COIN_ENABLED = 1;
+    private final int ENEMY_ENABLED = 2;
+    private final int ERASER_ENABLED = 2;
+
+    private int toolEnabled = 0;
+
     @FXML
     public void initialize() {
         gc = canvas.getGraphicsContext2D();
@@ -37,15 +45,55 @@ public class CreateLevelController {
         }
     }
 
+    @FXML
     public void chooseTile() {
+        toolEnabled = TILE_ENABLED;
+        updateGui();
     }
 
+    @FXML
     public void chooseCoin() {
+        toolEnabled = COIN_ENABLED;
+        updateGui();
     }
 
+    @FXML
     public void chooseEnemy() {
+        toolEnabled = ENEMY_ENABLED;
+        updateGui();
+
     }
 
+    @FXML
+    public void chooseEraser(ActionEvent actionEvent) {
+        toolEnabled = ERASER_ENABLED;
+        updateGui();
+    }
+
+    @FXML
     public void openLoadFile() {
+    }
+
+    @FXML
+    public void stepBackward(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void stepForward(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void openSaveFile(ActionEvent actionEvent) {
+    }
+
+    public void updateGui() {
+        String border = "-fx-border-color: green;"
+                + "-fx-border-width: 2;\n";
+        String noBorder = "-fx-border-color: none;\n"
+                + "-fx-border-width: 0;\n";
+
+        if (toolEnabled == TILE_ENABLED) {
+            tileButton.setStyle(tileButton.getStyle() + border);
+        }
     }
 }
