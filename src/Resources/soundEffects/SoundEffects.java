4f055535284ee9810f102097e7dc0e726741ee95
TheKingDule <dusjak@live.com>
2018-02-19 17:54:58 +0100

    - CollisonHandler updated - All sound effects added (some are commented out - need better sound effects before use)

--- src/Resources/soundEffects/SoundEffects.java
+++ src/Resources/soundEffects/SoundEffects.java
@@ -9,11 +9,15 @@ public enum SoundEffects {
 
     COIN("./Resources/music/coinCollect.wav"),
     HIT("./Resources/music/hit.wav"),
-    GAMEOVER("./Resources/music/gameOver.wav");
-    //STEP("./Resources/music/step.wav");
+    GAMEOVER("./Resources/music/gameOver.wav"),
+    RUN("./Resources/music/steps.wav"),
+    JUMP("./Resources/music/jump.wav"),
+    LANDING("./Resources/music/landing.wav");
 
+    public static boolean mute;
     private Clip effectClip;
-    private Preferences preferences = Preferences.userRoot();
+    double gain = 0.25;
+    float db = (float) (Math.log(gain)/Math.log(10.0) *20.0);
 
     SoundEffects(String soundFileName) {
         try {
@@ -31,9 +35,26 @@ public enum SoundEffects {
     }
 
     public void play() {
+        if(!mute) {
             effectClip.stop();
             effectClip.setFramePosition(0);
             effectClip.start();
+        }
     }
 
+    public void playLoop() {
+        if (!mute) {
+            if (!effectClip.isRunning()) {
+                FloatControl volume = (FloatControl) effectClip.getControl(FloatControl.Type.MASTER_GAIN);
+                volume.setValue(db);
+                effectClip.stop();
+                effectClip.setFramePosition(0);
+                effectClip.start();
+            }
+        }
+    }
+
+    public void stopLoop(){
+        effectClip.stop();
+    }
 }
