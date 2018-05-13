b1f6d9d47b173883d32376f6dfe71f0d3f6a5aa3
brandhaug-desktop <mbrandhaug@live.no>
2018-05-13 23:58:31 +0200

    MapParser wrong test

--- src/CreateLevel/MapParserTest.java
+++ src/CreateLevel/MapParserTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 
 public class MapParserTest {
     @Test
-    public void testGetValueFromFileHeader() {
+    public void testCorrectGetValueFromFileHeader() {
         try {
             File tempFile = File.createTempFile("temp", "txt");
 
@@ -28,6 +28,29 @@ public class MapParserTest {
             assertEquals(30, width);
             assertEquals(40, height);
             assertEquals(2, level);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testWrongGetValueFromFileHeader() {
+        try {
+            File tempFile = File.createTempFile("temp", "txt");
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+            bw.write("width30 heiht: 40 level 2");
+            bw.close();
+
+            MapParser mapParser = new MapParser();
+            int width = mapParser.getValueFromFileHeader(tempFile, "width");
+            int height = mapParser.getValueFromFileHeader(tempFile, "height");
+            int level = mapParser.getValueFromFileHeader(tempFile, "level");
+
+            assertEquals(-1, width);
+            assertEquals(-1, height);
+            assertEquals(-1, level);
 
         } catch (IOException e) {
             e.printStackTrace();
